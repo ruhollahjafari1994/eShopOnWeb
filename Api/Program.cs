51c8c84728087fcc49b1867829d4165236bbdd1f
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2022-04-29 19:35:31 +0430

    Add UsersController down grade MediatR to v9 from v10 to fix bug

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,8 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Reflection;
+ 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +10,87 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(current =>
+{
+	// **************************************************
+	// Set the comments path for the Swagger JSON and UI.
+	// File Name: Application.xml
+	var xmlFile =
+		$"{ System.Reflection.Assembly.GetExecutingAssembly().GetName().Name }.xml";
 
+	var xmlPathName =
+		System.IO.Path.Combine(System.AppContext.BaseDirectory, xmlFile);
+
+	current.IncludeXmlComments(filePath: xmlPathName);
+	// **************************************************
+
+	current.SwaggerDoc
+		(name: "v1",
+		info: new Microsoft.OpenApi.Models.OpenApiInfo
+		{
+			Version = "v1",
+			Title = "Application",
+
+			Description = "My Description",
+
+			TermsOfService =
+				new System.Uri("https://DTApp.ir/"),
+
+			Contact = new Microsoft.OpenApi.Models.OpenApiContact
+			{
+				Name = "Ruhollah Jafari",
+				Email = "jruhollah@gmail.com",
+				Url = new System.Uri("https://www.linkedin.com/in/ruhollah-jafari/"),
+			},
+
+			License = new Microsoft.OpenApi.Models.OpenApiLicense
+			{
+				Name = "Use under MIT",
+				Url = new System.Uri("https://blog.georgekosmidis.net/privacy-policy/"),
+			},
+		});
+});
+// **************************************************
+builder.Services.AddTransient
+	<Microsoft.AspNetCore.Http.IHttpContextAccessor,
+	Microsoft.AspNetCore.Http.HttpContextAccessor>();
+
+builder.Services.AddTransient
+	(serviceType: typeof(Dtat.Logging.ILogger<>),
+	implementationType: typeof(Dtat.Logging.NLogAdapter<>));
+// **************************************************
+
+// **************************************************
+builder.Services.AddTransient<Persistence.DatabaseContext>();
+// **************************************************
+
+// **************************************************
+// using MediatR;
+//services.AddMediatR(typeof(Startup));
+
+// using System.Reflection;
+builder.Services.AddMediatR(typeof
+	(Application.Users.EventHandlers.UserPasswordChangedEventHandler)
+	.GetTypeInfo().Assembly);
+// **************************************************
+
+// **************************************************
+builder.Services.AddTransient<Persistence.IUnitOfWork, Persistence.UnitOfWork>();
+// **************************************************
+IConfiguration Configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json")
+							.Build(); ;
+
+// **************************************************
+builder.Services.AddDbContext<Persistence.DatabaseContext>(options =>
+{
+	options.UseSqlServer(Configuration["Database:ConnectionString"]);
+});
+// **************************************************
+
+// **************************************************
+// **************************************************
+// **************************************************
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +107,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+});
--- Application/Users/EventHandlers/UserPasswordChangedEventHandler.cs
+++ Application/Users/EventHandlers/UserPasswordChangedEventHandler.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Users.EventHandlers
+﻿namespace Application.Users.EventHandlers
 {
-    internal class UserPasswordChangedEventHandler
-    {
-    }
+	public sealed class UserPasswordChangedEventHandler :
+MediatR.INotificationHandler<Domain.Aggregates.Users.Events.UserPasswordChangedEvent>
+	{
+		public UserPasswordChangedEventHandler
+			(Dtat.Logging.ILogger<UserPasswordChangedEventHandler> logger) : base()
+		{
+			Logger = logger;
+		}
+
+		private Dtat.Logging.ILogger<UserPasswordChangedEventHandler> Logger { get; }
+
+		public System.Threading.Tasks.Task Handle
+			(Domain.Aggregates.Users.Events.UserPasswordChangedEvent notification,
+			System.Threading.CancellationToken cancellationToken)
+		{
+			Logger.LogInformation($"{nameof(UserPasswordChangedEventHandler)} Done!");
+
+			return System.Threading.Tasks.Task.CompletedTask;
+		}
+	}
 }
